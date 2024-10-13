18cc389083b11c5ce40b80428134c9bc96db583e
Maveronic <mmesomamgbemena@yahoo.com>
2024-10-13 19:34:39 +0300

    Fixed file structure

---
+++ Working Tools/ghostmode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# The script adds a whitespace in your terminal allowing you to run commands without them being saved in the history folder.
+
+PROMPT_COMMAND_STRING='PROMPT_COMMAND=\'READLINE_LINE=" $READLINE_LINE"; READLINE_POINT=${#READLINE_LINE}\''
+
+# Check if the .bashrc file already contains PROMPT_COMMAND modification
+if grep -q "$PROMPT_COMMAND_STRING" ~/.bashrc; then
+    echo "You are already a ghost"
+else
+    # Append the PROMPT_COMMAND to the .bashrc file
+    echo "$PROMPT_COMMAND_STRING" >> ~/.bashrc
+    echo "PROMPT_COMMAND has been added to ~/.bashrc"
+
+    # Source the .bashrc to apply changes immediately
+    source ~/.bashrc
+    echo "Ghost mode"
+fi
